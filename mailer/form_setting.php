a61d0de456ecf8046db4eedc46c06ef20096bf2d
clrestrepo <clestrepo@live.com>
2020-07-16 09:56:17 -0500

    commit inicial

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "Nuevo mensaje de tu sitio clestrepo'"; //letter subject
+	$to = 'clestrepo@live.com'; // Enter Your E-mail
+	$from = 'clestrepo@live.com'; // Admin e-mail
+	$fromName = 'clestrepo'; // Your company name
+	$charset = 'UTF-8';
+?>
