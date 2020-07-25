d6a5ff7c3a37594e048e11c031eb0c2c9770c6f9
clrestrepo <clestrepo@live.com>
2020-07-25 13:17:11 -0500

    elimino archivos innecesarios

--- mailer/form_setting.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/*Form settings*/
-	$subj = "Nuevo mensaje de tu sitio clestrepo'"; //letter subject
-	$to = 'clestrepo@live.com'; // Enter Your E-mail
-	$from = 'clestrepo@live.com'; // Admin e-mail
-	$fromName = 'clestrepo'; // Your company name
-	$charset = 'UTF-8';
-?>
